382fa80f8c9912ffe76610a023032e78a9a33ac1
Andrey <strilkts@gmail.com>
2021-01-21 01:38:13 +0200

    fixed

--- hw_3_js/scripts/index.js
+++ hw_3_js/scripts/index.js
@@ -11,9 +11,12 @@ if (number >= 0) {
 // Second exercise
 let n = 50;
 let sum = 0;
+let count = 0;
 for (let i = 0; i <= n; i++){
-	if (i % 5 == 0){
-		sum +=i
+	if (i % 5 == 0 && count <= 10){
+		sum +=i;
+		count +=1;
+
 	}
 }
 
@@ -24,8 +27,9 @@ console.log("Summa = ",sum)
 let arr = [254, 115, 78, 25, 91, 45, 37];
 let new_arr = [];
 for (i=0; i < arr.length; i++){
-	if (arr[i] > 50){
-		new_arr = new_arr.concat(arr[i])
+	let number = arr[i];
+	if (number > 50){
+		new_arr = new_arr.concat(number)
 	}
 }
 console.log(new_arr);
